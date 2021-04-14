3e288e804f465c7fa1363ebb7e813430387784f9
Sam Slocum <sslocum@swri.org>
2021-04-14 06:18:45 -0500

    Connected simbad, added rotation

--- src/PySimGrism.py
+++ src/PySimGrism.py
@@ -6,6 +6,8 @@ from astroquery.skyview import SkyView
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.transforms as mtransforms
+import argparse
 
 class SimbadQuery:
     def __init__(self, target_name):
@@ -19,11 +21,26 @@ class SimbadQuery:
 
 
 def main():
-    query = SimbadQuery("vega")
+    parser = argparse.ArgumentParser("Grism simulator")
+    parser.add_argument("target", help="Simbad target string")
+    parser.add_argument("rotation", help="Rotation of image in degrees", type=float)
+    args = parser.parse_args()
+
+    query = SimbadQuery(args.target)
 #    paths = SkyView.get_images(position="vega", survey=["UVOT UVM2 Intensity"])
-    paths = SkyView.get_images(position='22:57:00,62:38:00',survey=['DSS2 Blue','DSS2 IR','DSS2 Red'],pixels='2400,2400',coordinates='J2000',grid=True,gridlabels=True)
+    rotation = args.rotation
+    position_str = str(query.ra.data[0]) + "," + str(query.dec.data[0])
+    print(position_str)
+#    paths = SkyView.get_images(position=position_str,survey=['DSS2 Blue','DSS2 IR','DSS2 Red'],pixels='2400,2400',coordinates='J2000',grid=True,gridlabels=True)
+    paths = SkyView.get_images(position=position_str,survey=['UVOT UVM2 Intensity'],pixels='2400,2400',coordinates='J2000',grid=True,gridlabels=True)
     print(paths)
-    plt.imshow(paths[0][0].data)
+    fig, ax = plt.subplots()
+    im = ax.imshow(paths[0][0].data)
+    trans_data = mtransforms.Affine2D().rotate_deg(rotation) + ax.transData
+    im.set_transform(trans_data)
+    x1, x2, y1, y2 = im.get_extent()
+    ax.plot([x1, x2, x2, x1, x1], [y1, y1, y2, y2, y1], "y--", transform=trans_data)
+
     print(paths[0][0].data)
     plt.show()
 if __name__=="__main__":
